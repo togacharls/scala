6c674dfbfd8724a5ec8197ff9a5f62a635f56f09
Carlos Torres García <togacharls@gmail.com>
2017-09-27 20:00:27 +0200

    HelloWorld

--- src/Main.scala
+++ src/Main.scala
@@ -1,9 +1,5 @@
 object Main extends App{
-
-  def square(x: Double) = x*x
-  def area(radius: Double): Double = 3.14159 * square(radius)
-
   override def main(args: Array[String]): Unit ={
-    println(this.area(10))
+    println("Hello World!")
   }
 }
