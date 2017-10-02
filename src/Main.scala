c5140b7752793f5faf87418ce9c0f7536ea542fb
Carlos Torres García <togacharls@gmail.com>
2017-10-02 20:24:47 +0200

    Frutería, a project with a list, a for loop and match example.

--- src/Main.scala
+++ src/Main.scala
@@ -1,7 +1,16 @@
 object Main extends App{
-
-
   override def main(args: Array[String]): Unit ={
-    println("Hello World!")
+    val array = List("Pepino", "Banana", "Zanahoria", "Calabacín");
+    var i = 0
+    var item:String = ""
+    for(item <- array){
+      i = i + 1
+      item match{
+        case "Pepino" => println(s"${i} ${item}")
+        case "Zanahoria" => println(s"${i} ${item}s")
+        case "Banana" => println(s"No quedan ${item}s")
+        case "Calabacín" => println(s"El ${item} esta muy rico")
+      }
+    }
   }
 }
