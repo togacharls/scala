bf19e6a56464453f15ed5688a0742cdf44b41783
Carlos Torres García <togacharls@gmail.com>
2017-10-04 19:31:55 +0200

    Factorial, a simple recursive example

--- src/Main.scala
+++ src/Main.scala
@@ -1,7 +1,15 @@
 object Main extends App{
 
+  def factorial(n: Int): Integer ={
+    if(n > 1)
+      return (n * factorial(n-1))
+    else
+      return 1
+  }
 
   override def main(args: Array[String]): Unit ={
-    println("Hello World!")
+    println (this.factorial(12))
+    println (this.factorial(10))
+    println (this.factorial(-1))
   }
 }
